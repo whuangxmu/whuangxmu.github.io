13123faec55ebd438fb770282e509bfc4086539b
Wei HUANG <whuang@xmu.edu.cn>
2024-11-18 23:18:50 +0800

    reorganize UI

--- assets/js/ui.js
+++ assets/js/ui.js
@@ -1,4 +1,4 @@
-function loadScript(url, id = null, callback = null) {
+function loadScript(url, callback = null, id = null) {
     var script = document.createElement("script");
     script.type = "text/javascript";
     if (id)
@@ -35,6 +35,20 @@ function loadCss(url) {
     document.head.appendChild(link);
 }
 
+function is_lower_than_ie9() {
+    var userAgent = window.navigator.userAgent;
+    var msie = userAgent.indexOf('MSIE ');
+    var trident = !!userAgent.match(/Trident\/7\.0/); // IE11 has different userAgent string
+    var rv = userAgent.indexOf('rv:');
+
+    if (msie > -1 || (trident && rv > -1 && parseInt(userAgent.substring(rv + 3, userAgent.indexOf('.', rv)), 10) <= 11)) {
+        // If IE (any version) is detected, check the version number
+        var ieVersion = parseInt(userAgent.substring(msie + 5, userAgent.indexOf('.', msie)), 10);
+        return ieVersion <= 8;
+    }
+    return false;
+}
+
 function load_scripts_and_csses() {
     loadCss("/static/bootstrap/3.4.1/css/bootstrap.css");
     loadCss("/static/animate/animate.css");
@@ -42,19 +56,25 @@ function load_scripts_and_csses() {
     loadCss("/assets/css/docs.navy.min.css");
     loadCss("/assets/css/patch.css");
     loadCss("//cdn.bootcdn.net/ajax/libs/font-awesome/6.4.2/css/all.css");
-    loadScript("/static/jquery/jquery-3.3.1.min.js");
-    loadScript("/static/bootstrap/3.4.1/js/bootstrap.min.js");
-    loadScript("/static/animate/pace.min.js");
-    loadScript("/static/animate/wow.min.js");
-    loadScript("/static/animate/classie.js");
-    loadScript("/static/animate/cbpAnimatedHeader.min.js");
-    loadScript("/static/jquery/jquery.placeholder.js");
-    loadScript("/static/polyfill/polyfill.min.js?features=es6");
-    loadScript("/static/animate/inspinia.js");
-    loadScript("/assets/js/docs.min.js");
-    loadScript("/assets/js/footer.min.js");
-    loadScript("/assets/js/ie10-viewport-bug-workaround.min.js");
-    loadScript("/assets/js/ie-emulation-modes-warning.min.js");
+    if (is_lower_than_ie9()) {
+        loadScript("/static/jquery/jquery-1.12.4.min.js");
+        loadScript("/static/ie8/html5shiv.min.js");
+        loadScript("/static/ie8/respond.min.js");
+        loadScript("/assets/js/ie8-responsive-file-warning.js");
+    } else {
+        // loadScript("/static/jquery/jquery-3.3.1.min.js");
+        loadScript("/static/bootstrap/3.4.1/js/bootstrap.min.js");
+        loadScript("/static/animate/pace.min.js");
+        loadScript("/static/animate/wow.min.js");
+        loadScript("/static/animate/classie.js");
+        loadScript("/static/animate/cbpAnimatedHeader.min.js");
+        loadScript("/static/jquery/jquery.placeholder.js");
+        loadScript("/static/polyfill/polyfill.min.js?features=es6");
+        loadScript("/static/animate/inspinia.js");
+        loadScript("/assets/js/docs.min.js");
+        loadScript("/assets/js/ie10-viewport-bug-workaround.min.js");
+        loadScript("/assets/js/ie-emulation-modes-warning.min.js");
+    }
 }
 
 // <script type="text/javascript" async="async" src="//cdn.bootcdn.net/ajax/libs/mathjax/3.1.2/es5/tex-chtml-full.min.js"></script>
@@ -196,35 +216,77 @@ function add_main() {
     var mainScript = document.getElementById('main');
     if (!mainScript)
         return;
-    var scriptContent=mainScript.innerHTML;
+    var scriptContent = mainScript.innerHTML;
     var mainTag = document.createElement("main");
-    mainTag.innerHTML = scriptContent;
-    if (typeof page_docs_header_title !== 'undefined') {
+    var config_page_style = 'doc';
+    if (typeof page_style !== 'undefined')
+        config_page_style = page_style;
+    if (config_page_style !== 'home') {
         mainTag.classList.add("container");
         mainTag.classList.add("bs-docs-container");
     }
+    if (config_page_style === 'doc-menu') {
+        const row = document.createElement("div");
+        row.className = "row";
+        const colMain = document.createElement("div");
+        colMain.className = "col-md-9";
+        colMain.setAttribute("role", "main");
+        colMain.innerHTML = scriptContent; // 设置innerHTML
+        const colComplementary = document.createElement("div");
+        colComplementary.className = "col-md-3";
+        colComplementary.setAttribute("role", "complementary");
+        const nav = document.createElement("nav");
+        nav.className = "bs-docs-sidebar hidden-print hidden-xs hidden-sm affix-top";
+        colComplementary.appendChild(nav);
+        row.appendChild(colMain);
+        row.appendChild(colComplementary);
+        mainTag.appendChild(row);
+    }else
+    {
+        mainTag.innerHTML = scriptContent;
+    }
     document.body.appendChild(mainTag);
+    loadScript("/assets/js/footer.min.js");
 }
 
 function add_page_loader() {
     var page_loader = document.createElement("div");
-    page_loader.innerHTML = `<div id="page-preloader"><div class="spinner"><div class="dot1"></div><div class="dot2"></div></div></div>`;
+    page_loader.innerHTML = `
+<style>
+@media screen {
+    #page-preloader {
+        width: 100%;
+        height: 100%;
+        position: fixed;
+        top: 0;
+        left: 0;
+        background: #fff;
+        z-index: 99999;
+        text-align: center
+    }
+}
+</style>
+<div id="page-preloader"><div class="spinner"><div class="dot1"></div><div class="dot2"></div></div></div>`;
     document.body.appendChild(page_loader);
 }
 
+function add_title() {
+    var title = document.createElement("title");
+    if (typeof myoj2_app_name === 'undefined')
+        return;
+    title.innerHTML = myoj2_app_name;
+    document.head.appendChild(title);
+}
+
 function load_page() {
-    load_scripts_and_csses();
     add_page_loader();
+    load_scripts_and_csses();
+    add_title();
     add_skippy();
     add_header();
     add_docs_header_after_header();
     add_main();
     add_footer();
-    setTimeout(function () {
-        $(document.body).scrollspy({target: ".bs-docs-sidebar"});
-        $(document.body).scrollspy("refresh");
-        $(document.body).scrollspy("activate");
-    }, 1000);
 }
 
 load_page();
